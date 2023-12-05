bf9bff47a8a01b86260b4a959734b5631412e136
AhXaing <ahxaing09@gmail.com>
2023-12-05 18:50:41 +0700

    updated code

--- node-api/src/util/db.js
+++ node-api/src/util/db.js
@@ -1,8 +1,13 @@
-const mysql = require("mysql2");
+const mysql = require("mysql");
+const util = require("util");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
+  port: 5306,
   password: "",
   database: "ecom_db",
 });
+// promis wrapper to enable async await with mysql
+db.query = util.promisify(db.query).bind(db);
+
 module.exports = db;
